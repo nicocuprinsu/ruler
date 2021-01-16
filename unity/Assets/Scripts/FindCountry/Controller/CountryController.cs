8019145ee9a71be37c9c2a240f22e31be3f705c7
nicocuprinsu <n.cuprinsu@student.tue.nl>
2021-01-16 19:33:05 +0100

    merge

--- unity/Assets/Scripts/FindCountry/Controller/CountryController.cs
+++ unity/Assets/Scripts/FindCountry/Controller/CountryController.cs
@@ -11,6 +11,8 @@
 	using Util.Geometry.Trapezoidal;
 	using General.Controller;
 	using UnityEngine.SceneManagement;
+	using UnityEngine.UI;
+	using UnityEngine.EventSystems;
 
 	public class CountryController : MonoBehaviour
 	{
@@ -18,36 +20,65 @@
 
 		[SerializeField]
 		private GameObject m_borderMeshPrefab;
+
 		[SerializeField]
 		private ButtonContainer m_advanceButton;
 
 		[SerializeField]
 		private List<CountryLevel> m_levels;
+
 		[SerializeField]
 		private string m_victoryScene;
+
 		[SerializeField]
 		private GameObject m_pointPrefab;
+
+		[SerializeField]
+		private Text m_levelText;
+
+		[SerializeField]
+		private Text m_tryagainText;
+
+		[SerializeField]
+		private Image m_tryagainContainer;
+
+		[SerializeField]
+		private Text m_boromirExceptionText;
+
+		[SerializeField]
+		private Image m_boromirExceptionContainer;
+
+		private string levelCountry;
+		private CountryPoint selectedCountry;
+		private string searchResult;
+		private bool displayDecomposition = false;
 
 		private List<LineSegment> m_segments;
 
 		private List<GameObject> instantObjects;
+		private List<GameObject> instantTrapezoidObjects;
 
-		protected int m_levelCounter = 0;
+		protected int m_levelCounter = -1;
 
-		private List<Vector2> PointsTest;
-
-		private List<CountryPoint> m_points;
-
-		private List<CountryLineSegment> map = new List<CountryLineSegment>();
+		private List<CountryLineSegment> map;
 
 		private TrapezoidalDecomposition decomp;
 
 		// Use this for initialization
 		void Start()
 		{
+			// get unity objects
+			m_segments = new List<LineSegment>();
+			instantObjects = new List<GameObject>();
+			instantTrapezoidObjects = new List<GameObject>();
+			m_advanceButton.Disable();
+			m_tryagainContainer.gameObject.SetActive(false);
+			m_tryagainText.gameObject.SetActive(false);
+			m_boromirExceptionContainer.gameObject.SetActive(false);
+			m_boromirExceptionText.gameObject.SetActive(false);
 
-
-            map.Add(new CountryLineSegment(new Vector2(0.27f, 0.3178717598908595f), new Vector2(0.32f, 0.41336971350613916f), "null"));
+			map = new List<CountryLineSegment>();
+			map.Add(new CountryLineSegment(new Vector2(0.27f, 0.3178717598908595f), new Vector2(0.32f, 0.41336971350613916f), "null"));
             map.Add(new CountryLineSegment(new Vector2(0.32f, 0.41336971350613916f), new Vector2(0.3f, 0.48158253751705316f), "Gondor"));
             map.Add(new CountryLineSegment(new Vector2(0.27f, 0.3178717598908595f), new Vector2(0.45f, 0.3997271487039563f), "Gondor"));
             map.Add(new CountryLineSegment(new Vector2(0.3f, 0.48158253751705316f), new Vector2(0.34f, 0.5361527967257844f), "Eriador"));
@@ -83,30 +114,64 @@
 
             decomp = new TrapezoidalDecomposition(map);
 
-			m_advanceButton.Disable();
-
 			// get unity objects
-			m_segments = new List<LineSegment>();
-			m_points = new List<CountryPoint>();
 			instantObjects = new List<GameObject>();
+            instantTrapezoidObjects = new List<GameObject>();
 
+			// Display segments on screen.
 			foreach (CountryLineSegment seg in map)
 			{
 				AddSegment(seg);
 			}
 
-            DisplayTrapezoidalDecomposition();
-        }
+			AdvanceLevel();
+		}
 
-		public void AddSegment(CountryLineSegment segment)
+		public void AdvanceLevel()
 		{
-			m_segments.Add(segment.Segment);
+			m_levelCounter++;
 
+			// clear old level
+			m_tryagainContainer.gameObject.SetActive(false);
+			m_tryagainText.gameObject.SetActive(false);
+			m_advanceButton.Disable();
+
+			if (selectedCountry != null)
+			{
+				Destroy(selectedCountry.gameObject);
+			}
+
+			if (m_levelCounter < m_levels.Count)
+			{
+				InitLevel();
+			}
+			else
+			{
+				SceneManager.LoadScene(m_victoryScene);
+			}
+		}
+
+		public void InitLevel()
+		{
+			// create new level
+			var level = m_levels[m_levelCounter];
+			levelCountry = level.Country;
+
+			// update text box
+			m_levelText.text = "Conquer " + levelCountry + "!";
+		}
+
+        // Displays new segment on the map
+		public void AddSegment(CountryLineSegment segment, List<GameObject> objects = null)
+		{
 			// instantiate new road mesh
 			var roadmesh = Instantiate(m_borderMeshPrefab, Vector3.forward, Quaternion.identity) as GameObject;
 			roadmesh.transform.parent = this.transform;
-			instantObjects.Add(roadmesh);
-
+            if (objects != null)
+            {
+				objects.Add(roadmesh);
+			}
+			
 			Vector3 p1 = Camera.main.ViewportToWorldPoint(segment.Point1);
 			Vector3 p2 = Camera.main.ViewportToWorldPoint(segment.Point2);
 
@@ -114,39 +179,90 @@
 			roadmeshScript.CreateNewMesh(p1, p2);
 		}
 
-		public void AddSegment(Vector3 a_point1, Vector3 a_point2)
-		{
-			var segment = new LineSegment(a_point1, a_point2);
-
-			m_segments.Add(segment);
-
-			// instantiate new road mesh
-			var roadmesh = Instantiate(m_borderMeshPrefab, Vector3.forward, Quaternion.identity) as GameObject;
-			roadmesh.transform.parent = this.transform;
-			instantObjects.Add(roadmesh);
-
-			roadmesh.GetComponent<CountrySegment>().Segment = segment;
-
-			var roadmeshScript = roadmesh.GetComponent<ReshapingMesh>();
-			roadmeshScript.CreateNewMesh(a_point1, a_point2);
-		}
-
-		public void InitLevel()
-		{
-		}
-
 		// Update is called once per frame
 		void Update()
 		{
-			if (Input.GetMouseButtonDown(0))
+
+			if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
 			{
+				// remove previous selected point
+				if (selectedCountry != null)
+				{
+					Destroy(selectedCountry.gameObject);
+				}
+
+				// instantiate new selected point and display point prefab
+				var worldlocationDisplay = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+				worldlocationDisplay.z = -2f;
+				var go = Instantiate(m_pointPrefab, worldlocationDisplay, Quaternion.identity) as GameObject;
+				selectedCountry = FindObjectsOfType<CountryPoint>()[0];
+				selectedCountry.Pos = worldlocationDisplay;
+				Debug.Log(selectedCountry.Pos);
+
 				var worldlocation = Input.mousePosition;
 				worldlocation.x /= Screen.width;
 				worldlocation.y /= Screen.height;
-				worldlocation.z = -2f;
-				Trapezoid res = (Trapezoid)decomp.SearchGraph.Search(worldlocation).Value;
-                Debug.Log(res.Bottom.Country);
-            }
+				m_tryagainText.gameObject.SetActive(false);
+				m_tryagainContainer.gameObject.SetActive(false);
+				Debug.Log(worldlocation);
+				CheckSolution(worldlocation);
+			}
+
+			if (Input.GetKeyDown(KeyCode.T))
+			{
+				if (!displayDecomposition)
+				{
+					DisplayTrapezoidalDecomposition();
+					displayDecomposition = true;
+				}
+				else
+				{
+					RemoveTrapezoidalDecomposition();
+					displayDecomposition = false;
+				}
+
+			}
+		}
+
+		public void CheckSolution(Vector3 worldlocation)
+		{
+			// retrieve which country has been clicked by the user from trapezoidal decomp
+			Trapezoid res = (Trapezoid)decomp.SearchGraph.Search(worldlocation).Value;
+			searchResult = res.Bottom.Country;
+			if (searchResult == levelCountry)
+			{
+				if (levelCountry == "Mordor")
+				{
+					m_boromirExceptionContainer.gameObject.SetActive(true);
+					m_boromirExceptionText.gameObject.SetActive(true);
+				}
+				m_advanceButton.Enable();
+			}
+			else
+			{
+				m_tryagainContainer.gameObject.SetActive(true);
+				m_tryagainText.gameObject.SetActive(true);
+				m_advanceButton.Disable();
+				if (searchResult == "null")
+				{
+					m_tryagainText.text = "Try again! You clicked outside the map!";
+				}
+				else
+				{
+					m_tryagainText.text = "Try again! You selected " + searchResult.ToString() + "!";
+				}
+			}
+		}
+
+		public void RemoveTrapezoidalDecomposition()
+		{
+			// destroy game objects created in level
+			foreach (var obj in instantTrapezoidObjects)
+			{
+				// destroy immediate
+				// since controller will search for existing objects afterwards
+				DestroyImmediate(obj);
+			}
 		}
 
 		public void DisplayTrapezoidalDecomposition()
@@ -162,7 +278,7 @@
 					{
 						LineSegment intersect = new LineSegment(t.RightPoint, (Vector2)t.Top.Segment.Intersect(up));
 
-						AddSegment(new CountryLineSegment(intersect));
+						AddSegment(new CountryLineSegment(intersect), instantTrapezoidObjects);
 					}
 				}
 
@@ -173,7 +289,7 @@
 					{
 						LineSegment intersect = new LineSegment(t.RightPoint, (Vector2)t.Bottom.Segment.Intersect(down));
 
-						AddSegment(new CountryLineSegment(intersect));
+						AddSegment(new CountryLineSegment(intersect), instantTrapezoidObjects);
 					}
 				}
 
@@ -184,7 +300,7 @@
 					{
 						LineSegment intersect = new LineSegment(t.LeftPoint, (Vector2)t.Top.Segment.Intersect(up));
 
-						AddSegment(new CountryLineSegment(intersect));
+						AddSegment(new CountryLineSegment(intersect), instantTrapezoidObjects);
 					}
 				}
 
@@ -195,26 +311,10 @@
 					{
 						LineSegment intersect = new LineSegment(t.LeftPoint, (Vector2)t.Bottom.Segment.Intersect(down));
 
-						AddSegment(new CountryLineSegment(intersect));
+						AddSegment(new CountryLineSegment(intersect), instantTrapezoidObjects);
 					}
 				}
 			}
-		}
-
-		/// <summary>
-		/// Clears hull and relevant game objects
-		/// </summary>
-		private void Clear()
-		{
-			//m_segments.Clear();
-
-			//// destroy game objects created in level
-			//foreach (var obj in instantObjects)
-			//{
-			//	// destroy immediate
-			//	// since controller will search for existing objects afterwards
-			//	DestroyImmediate(obj);
-			//}
 		}
 	}
 }
